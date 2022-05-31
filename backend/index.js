bed2f51e91e4753b27f54c1b840091085cdc601d
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2022-05-31 15:25:52 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
-// Crear app
+// Módulos
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// Conexión con la base (host, usuario, clave, y nombre de la db)
+// Esto habría que modificarlo al hostear en Heroku
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -12,10 +14,12 @@ const db = mysql.createPool({
     database: 'jardin',
 });
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Query para insertar consulta en base de datos
 app.post('/api/insert', (req, res) => {
 
     const nombre = req.body.nombre;
@@ -26,9 +30,11 @@ app.post('/api/insert', (req, res) => {
 
     const sql = "INSERT INTO consultas (nombre, apellido, correo, asunto, consulta) VALUES (?,?,?,?,?)";
     db.query(sql, [nombre, apellido, correo, asunto, consulta], (err, result) => {
-        console.log(err)
-    })
-})
+        console.log(err);
+    });
+});
+
+// Listen
 app.listen(3001, () => {
     console.log('El server está corriendo en el puerto 3001');
 });
