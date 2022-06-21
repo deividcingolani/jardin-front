03aff67cbea19c2b3b0b58a0d451f6575e2eb2cf
nicoschonfeld <yznicolass@gmail.com>
2022-06-21 19:15:41 -0300

    Save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,9 @@
-import './App.css';
-import React, { useState} from "react";
-import Axios from 'axios';
+import React from "react";
 
-function App() {
-
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [asunto, setAsunto] = useState('');
-  const [consulta, setConsulta] = useState('');
-
-  const enviarConsulta = () => {
-    Axios.post("http://localhost:3001/api/insert", {
-      _nombre: nombre, 
-      _apellido: apellido, 
-      _correo: correo, 
-      _asunto: asunto, 
-      _consulta: consulta
-    }).then( () => {
-      alert('Gracias por enviar su consulta.');
-    })
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <h1>Prueba</h1>
-
-      <div className="form">
-        
-        <input type="text" name="nombre" onChange={(e) => {
-          setNombre(e.target.value)
-        }}/>
-          
-        <input type="text" name="apellido" onChange={(e) => {
-          setApellido(e.target.value)
-        }}/>
-
-        <input type="text" name="correo" onChange={(e) => {
-          setCorreo(e.target.value)
-        }}/>
-
-        <input type="text" name="asunto" onChange={(e) => {
-          setAsunto(e.target.value)
-        }}/>
-
-        <input type="text" name="consulta" onChange={(e) => {
-          setConsulta(e.target.value)
-        }}/>
-
-        <button onClick = {enviarConsulta}>Submit</button>
-
-      </div>
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
-
-export default App;
