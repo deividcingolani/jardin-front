8408cc9b5f63692f665793da9754662a11c8f342
nicoschonfeld <yznicolass@gmail.com>
2022-06-21 23:06:40 -0300

    Implementación de rutas en el componente App.js

---
+++ frontend/src/Router/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar.js/Navbar";
+import Contact from "../pages/Contact";
+import Gallery from "../pages/Gallery";
+import Garden from "../pages/Garden";
+import Home from "../pages/Home";
+
+export default function Router() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/garden" element={<Garden />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<p>Error - 404</p>} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
